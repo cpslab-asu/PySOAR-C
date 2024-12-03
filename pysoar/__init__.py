0b86f71b7cefbc7d02f543fe3caf8899fba30d32
Quinn Thibeault <quinn.thibeault96@gmail.com>
2024-12-03 09:49:06 -0700

    Update top-level exports

--- pysoar/__init__.py
+++ pysoar/__init__.py
@@ -1,6 +1,4 @@
-from cProfile import run
-from .coreAlgorithm import PySOAR
-from .staliroIntegration import run_pysoar
+from .optimizer import PySOAR, run_pysoar
 
 __version__ = '0.1.0'
 __all__ = ["PySOAR", "run_pysoar"]
