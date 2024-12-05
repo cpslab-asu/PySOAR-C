7769da102b07b0312ff64bc36e479812fb48a21d
Quinn Thibeault <quinn.thibeault96@gmail.com>
2024-12-05 11:28:03 -0700

    Fix psy-taliro optimizer implementation

--- pysoarc/staliroIntegration/staliroIntegrate.py
+++ pysoarc/staliroIntegration/staliroIntegrate.py
@@ -1,14 +1,14 @@
 from dataclasses import dataclass
-from typing import Any, List, Sequence, Callable
+from typing import List
 from attr import frozen
 
 import numpy as np
 from numpy.typing import NDArray
-from staliro.core import Interval, Optimizer, ObjectiveFn, Sample
+from staliro.optimizers import Optimizer, ObjFunc
 
 from ..coreAlgorithm import PySOARC
+from ..gprInterface import GaussianProcessRegressor
 
-Bounds = Sequence[Interval]
 
 @frozen(slots=True)
 class PySOARCResult:
@@ -22,7 +22,7 @@ class PySOARCResult:
 
 
 @dataclass(frozen=True)
-class run_pysoarc(Optimizer[PySOARCResult, None]):
+class run_pysoarc(Optimizer[NDArray[np.double], PySOARCResult]):
     """The PySOARC optimizer"""
 
     # inpRanges: 
@@ -38,34 +38,31 @@ class run_pysoarc(Optimizer[PySOARCResult, None]):
     eps_tr: float
     min_tr_size: float
     TR_threshold: float
-    gpr_model: Callable
+    gpr_model: GaussianProcessRegressor
     local_search: str
-    behavior: str
+    behavior: PySOARC.Behavior = PySOARC.Behavior.MINIMIZATION
 
-    def optimize(self, func: ObjectiveFn, bounds: Bounds, budget:int, seed: int) -> PySOARCResult:
-        region_support = np.array((tuple(bound.astuple() for bound in bounds),))[0]
-        
-        def test_function(sample: np.ndarray) -> float:
-            return func.eval_sample(Sample(sample))
-        
-        samples = PySOARC(
-                        n_0=self.n_0, 
-                        nSamples=budget, 
-                        trs_max_budget=self.trs_max_budget,
-                        max_loc_iter=self.max_loc_iter,
-                        inpRanges= region_support,
-                        alpha_lvl_set=self.alpha_lvl_set, 
-                        eta0=self.eta0, 
-                        eta1=self.eta1, 
-                        delta=self.delta, 
-                        gamma=self.gamma, 
-                        eps_tr=self.eps_tr, 
-                        min_tr_size=self.min_tr_size,
-                        TR_threshold=self.TR_threshold,
-                        prob= test_function,
-                        gpr_model=self.gpr_model,
-                        seed = seed,
-                        local_search=self.local_search,
-                        behavior=self.behavior
-                )
+    def optimize(self, func: ObjFunc[NDArray[np.double]], params: Optimizer.Params) -> PySOARCResult:
+        region_support = np.array([bound for bound in params.input_bounds])
+        samples = PySOARC.PySOARC(
+            n_0=self.n_0, 
+            nSamples=params.budget, 
+            trs_max_budget=self.trs_max_budget,
+            max_loc_iter=self.max_loc_iter,
+            inpRanges= region_support,
+            alpha_lvl_set=self.alpha_lvl_set, 
+            eta0=self.eta0, 
+            eta1=self.eta1, 
+            delta=self.delta, 
+            gamma=self.gamma, 
+            eps_tr=self.eps_tr, 
+            min_tr_size=self.min_tr_size,
+            TR_threshold=self.TR_threshold,
+            test_fn=lambda sample: func.eval_sample(sample),
+            gpr_model=self.gpr_model,
+            seed=params.seed,
+            local_search=self.local_search,
+            behavior=self.behavior
+        )
+
         return PySOARCResult(samples)                                
