3962995a2fc2afe882e0878478feb6980d3c3aaa
Quinn Thibeault <quinn.thibeault96@gmail.com>
2024-12-20 08:01:32 -0700

    Begin refactor of GPR module. Remove wrapper and rename provided GPR implementation

--- src/soar/gpr.py
+++ src/soar/gpr.py
@@ -1,19 +1,22 @@
-from abc import ABC, abstractmethod
-from sklearn.gaussian_process import GaussianProcessRegressor
-from sklearn.gaussian_process.kernels import Matern, ConstantKernel, RBF, WhiteKernel
-from scipy.optimize import fmin_l_bfgs_b
-from sklearn.preprocessing import StandardScaler
-from warnings import catch_warnings
+from __future__ import annotations
+
 import warnings
+from abc import ABC, abstractmethod
+from typing import TYPE_CHECKING
+
+import numpy as np
+from scipy.optimize import fmin_l_bfgs_b
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import Matern
+from sklearn.preprocessing import StandardScaler
+
+if TYPE_CHECKING:
+    from numpy.typing import ArrayLike, NDArray
 
 
-class GaussianProcessRegressorStructure(ABC):
+class GPR(ABC):
     @abstractmethod
-    def __init__(self):
-        raise NotImplementedError
-
-    @abstractmethod
-    def fit_gpr(self, x_train, y_train):
+    def fit(self, x_train: NDArray, y_train: NDArray):
         """Method to fit gpr Model
 
         Attributes:
@@ -23,10 +26,11 @@ class GaussianProcessRegressorStructure(ABC):
 
         
         """
-        raise NotImplementedError
+
+        raise NotImplementedError()
 
     @abstractmethod
-    def predict_gpr(self, x_test):
+    def predict(self, x_test: NDArray) -> tuple[NDArray, NDArray]:
         """Method to predict mean and std_dev from gpr model
 
         Attributes:
@@ -39,15 +43,10 @@ class GaussianProcessRegressorStructure(ABC):
             mean
             std_dev
         """
-        raise NotImplementedError
 
+        raise NotImplementedError()
 
-
-class GPR:
-    def __init__(self, gpr_model) -> None:
-        self.gpr_model = gpr_model
-
-    def fit(self, x_train, y_train):
+    def do_fit(self, x_train: ArrayLike, y_train: ArrayLike):
         """ Wrapper to fit user defined gpr model
 
         Attributes:
@@ -61,16 +60,22 @@ class GPR:
             TypeError: If y_train is not (n,) numpy array
             TypeError: If there is a mismatch between x_train and y_train
         """
+
+        x_train = np.array(x_train)
+        y_train = np.array(y_train)
+
         if len(x_train.shape) != 2:
             raise TypeError(f"Received samples set input: Expected (n, dim) array, received {x_train.shape} instead.")
+
         if len(y_train.shape) != 1:
             raise TypeError(f"Received evaluations set input: Expected (n,) array, received {y_train.shape} instead.")
+
         if x_train.shape[0] != y_train.shape[0]:
             raise TypeError(f"x_train, y_train set mismatch. x_train has shape {x_train.shape} and y_train has shape {y_train.shape}")
 
-        self.gpr_model.fit_gpr(x_train, y_train)
+        self.fit(x_train, y_train)
 
-    def predict(self, X):
+    def do_predict(self, x_test: ArrayLike):
         """Wrapper to predict from user defined gpr model
 
         Attributes:
@@ -86,10 +91,13 @@ class GPR:
             mean
             std
         """
-        if len(X.shape) != 2:
-            raise TypeError(f"Received samples set input: Expected (n, dim) array, received {X.shape} instead.")
+        
+        x_test = np.array(x_test)
 
-        mean, std = self.gpr_model.predict_gpr(X)
+        if len(x_test.shape) != 2:
+            raise TypeError(f"Received samples set input: Expected (n, dim) array, received {x_test.shape} instead.")
+
+        mean, std = self.predict(x_test)
 
         assert len(mean.shape) == 1, f"Mean from GPR should be of shape (n, ). Received {mean.shape} instead."
         assert len(std.shape) == 1, f"std_dev from GPR should be of shape (n, ). Received {std.shape} instead."
@@ -98,42 +106,42 @@ class GPR:
         return mean, std
 
 
-
-
-
 def optimizer_lbfgs_b(obj_func, initial_theta):
-    with catch_warnings():
+    with warnings.catch_warnings():
         warnings.simplefilter("ignore")
         params = fmin_l_bfgs_b(
-            obj_func, initial_theta, bounds=None, maxiter=30000, maxfun=1e10
+            obj_func, initial_theta, maxiter=30000, maxfun=int(1e10)
         )
     return params[0], params[1]
 
 
-class InternalGPR(GaussianProcessRegressorStructure):
+class DefaultGPR(GPR):
     def __init__(self, random_state = 12345):
-        self.gpr_model = GaussianProcessRegressor(
-            kernel=Matern(nu=2.5), alpha=1e-6, normalize_y=True, n_restarts_optimizer=5, random_state = random_state
-        )
         self.scale = StandardScaler()
+        self.model = GaussianProcessRegressor(
+            kernel=Matern(nu=2.5),
+            alpha=1e-6,
+            normalize_y=True,
+            n_restarts_optimizer=5,
+            random_state = random_state,
+        )
 
-    def fit_gpr(self, X, Y):
+    def fit(self, x_train: NDArray, y_train: NDArray):
         """Method to fit gpr Model
 
         Attributes:
         ----------
             x_train: Samples from Training set.
             y_train: Evaluated values of samples from Trainig set.
-
-        
         """
-        X_scaled = self.scale.fit_transform(X)
-        
-        with catch_warnings():
-            warnings.simplefilter("ignore")
-            self.gpr_model.fit(X_scaled, Y)
 
-    def predict_gpr(self, X):
+        x_scaled = self.scale.fit_transform(x_train)
+        
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            self.model.fit(x_scaled, y_train)
+
+    def predict(self, x_test: NDArray) -> tuple[NDArray, NDArray]:
         """Method to predict mean and std_dev from gpr model
 
         Attributes:
@@ -146,9 +154,12 @@ class InternalGPR(GaussianProcessRegressorStructure):
             mean
             std_dev
         """
-        x_scaled = self.scale.transform(X)
-        with catch_warnings():
+
+        x_scaled = self.scale.transform(x_test)
+
+        with warnings.catch_warnings():
             warnings.simplefilter("ignore")
-            yPred, predSigma = self.gpr_model.predict(x_scaled, return_std=True)
+            yPred, predSigma = self.model.predict(x_scaled, return_std=True)
+
         return yPred, predSigma
 
